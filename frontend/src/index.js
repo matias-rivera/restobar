29873a140ce1e6f41f9bd5154e8af121d1255ac1
Matías Rivera <juanmatiasrivera@outlook.com>
2020-11-03 09:47:11 -0300

    redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
